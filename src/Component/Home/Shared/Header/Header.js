dd2449df326158a4e35101d2b0402c617286793b
nayeem407 <sabbirahmednayeem941@gmail.com>
2022-01-25 09:17:08 +0600

    test push

---
+++ src/Component/Home/Shared/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Header() {
+  return <div>
+      <h2>this is header </h2>
+  </div>;
+}
+
+export default Header;
---
+++ src/Component/Home/Shared/Navber/Navber.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Navber() {
+  return <div>
+      <h2>this is navber</h2>
+  </div>;
+}
+
+export default Navber;
